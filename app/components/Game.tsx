a85a340b59f39d86038dc58a85e4297b0ffedffa
Tiago Machado <tyygo@live.com>
2026-08-21 14:27:40 -0300

    Ajustes na versão mobile, correção de bugs.

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -46,8 +46,10 @@ export default function Game() {
   }, []);
 
   return (
-    <div className="game-shell touch-none">
-      <div ref={containerRef} className="game-canvas-box" />
-    </div>
-  );
+  <div
+    ref={containerRef}
+    className="mx-auto w-full max-w-[800px] touch-none"
+    style={{ aspectRatio: "800 / 480" }}
+  />
+);
 }
